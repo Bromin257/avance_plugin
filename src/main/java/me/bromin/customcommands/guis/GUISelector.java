c840ad4ddd1fe539e66d10d5a067762121c09636
Bromin257 <bromin247@gmail.com>
2021-11-10 21:16:30 +0100

    10.11.21 - EOD
    - Config started for map list
    - Friend List Fixed

--- src/main/java/me/bromin/customcommands/guis/GUISelector.java
+++ src/main/java/me/bromin/customcommands/guis/GUISelector.java
@@ -1,21 +1,14 @@
 package me.bromin.customcommands.guis;
 
 import me.bromin.customcommands.Main;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.event.block.Action;
 
 import java.sql.*;
@@ -38,7 +31,7 @@ public class GUISelector implements Listener {
     public void openNewGui(Player p, int h) {
 
         if (h == 1) {
-            MapList.openMaps(p);
+            GamemodeList.openGamemodeList(p);
         } else if (h == 2) {
 
             FriendList.openFriendSelector(p);
--- src/main/java/me/bromin/customcommands/guis/MapList.java
+++ src/main/java/me/bromin/customcommands/guis/MapList.java
@@ -2,65 +2,17 @@ package me.bromin.customcommands.guis;
 
 import me.bromin.customcommands.Main;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.List;
-
-public class MapList implements Listener {
-
-
-
-    // Open maps according to the map type given by
-    public static void openMaps(Player p) {
-
-        int mapType;
-        Player player;
-        Inventory gui;
-
-        // Get config.yml file
+public class MapList {
+    public void openMapList(String gamemodeType) {
         FileConfiguration config = Main.getCustomConfig();
 
-        gui = Bukkit.createInventory(null, InventoryType.HOPPER, "Maps");
-
-        for (String key: config.getConfigurationSection("gamemodes").getKeys(false)) {
-            String m = config.getConfigurationSection("gamemodes").getString(key + ".itemtype");
-            String n = config.getConfigurationSection("gamemodes").getString(key + ".displayname");
-            String c = config.getConfigurationSection("gamemodes").getString(key + ".namecolor");
-            int l = config.getConfigurationSection("gamemodes").getInt(key + ".custommodeldata");
-            int i = config.getConfigurationSection("gamemodes").getInt(key + ".index");
+        Inventory gui = Bukkit.createInventory(null, 54, "Maps");
 
 
-            ItemStack item = new ItemStack(Material.valueOf(m), 1);
-            ItemMeta meta = item.getItemMeta();
-            meta.setDisplayName(ChatColor.valueOf(c) + n);
-            meta.setCustomModelData(l);
-            item.setItemMeta(meta);
-
-            gui.setItem(i, item);
-        }
-        p.openInventory(gui);
-    }
-
-
-        /*
-    public void gameGUI(int i) {
-
-        Inventory gui;
-
-        gui = Bukkit.createInventory(null, InventoryType.CHEST, "Adventure Maps");
-
-        player.openInventory(gui);
 
     }
-*/
-
-
 }
